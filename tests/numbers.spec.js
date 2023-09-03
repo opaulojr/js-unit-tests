7c8f87fc25451e30660d6156e5ac7d475d150095
opaulojr <paulojr.hs@gmail.com>
2023-09-03 18:12:57 -0300

    fix-numbers-tests

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -2,22 +2,22 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando recebe [1, 2, 3, 4, 5]', () => {
-    expect(numbers([1, 2, 3, 4, 5]).toBe(true));
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
   });
 
   it('Verifica se a função `numbers` retorna `false` quando recebe [1, 2, "3", 4, 5]', () => {
-    expect(numbers([1, 2, '3', 4, 5]).toBe(false));
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false);
   });
 
   it('Verifica se a função `numbers` retorna `false` quando recebe [1, "a", 3]', () => {
-    expect(numbers([1, 'a', 3]).toBe(false));
+    expect(numbers([1, 'a', 3])).toBe(false);
   });
 
   it('Verifica se a função `numbers` retorna `false` quando recebe [" "]', () => {
-    expect(numbers(['']).toBe(false));
+    expect(numbers([''])).toBe(false);
   });
 
   it('Verifica se a função `numbers` retorna `undefined` quando não recebe nenhum parâmetro', () => {
-    expect(numbers().toBeUndefined);
+    expect(numbers()).toBeUndefined();
   });
 });
