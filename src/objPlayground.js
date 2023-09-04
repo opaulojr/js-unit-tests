262e7441d2dede2f1e3385e18a0220ca570f0759
opaulojr <paulojr.hs@gmail.com>
2023-09-04 17:28:35 -0300

    implement-objPlayground-func

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -1,5 +1,21 @@
-const calculator = () => {};
+function calculator(a, b) {
+  return {
+    sum: a + b,
+    mult: a * b,
+    div: Math.floor(a / b),
+    sub: a - b,
+  };
+}
 
-const arrayGenerator = () => {};
+function arrayGenerator(type, obj) {
+  if (type === 'keys') {
+    return Object.keys(obj);
+  } if (type === 'values') {
+    return Object.values(obj);
+  } if (type === 'entries') {
+    return Object.entries(obj);
+  }
+  return [];
+}
 
 module.exports = { calculator, arrayGenerator };
