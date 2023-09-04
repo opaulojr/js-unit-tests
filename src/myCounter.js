27383ca26a5c103fc2b63aa947830506eef1d126
opaulojr <paulojr.hs@gmail.com>
2023-09-04 17:22:41 -0300

    implement-myCounter-func

--- src/myCounter.js
+++ src/myCounter.js
@@ -1,3 +1,12 @@
-const myCounter = () => {};
+const myCounter = () => {
+  const myArray = [];
+  for (let outerCounter = 0; outerCounter <= 3; outerCounter += 1) {
+    myArray.push(outerCounter);
+    for (let innerCounter = 2; innerCounter <= 3; innerCounter += 1) {
+      myArray.push(innerCounter);
+    }
+  }
+  return myArray;
+};
 
 module.exports = myCounter;
