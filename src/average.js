c2b3d259e5d2a4384592dd42b44bc1d92449943b
opaulojr <paulojr.hs@gmail.com>
2023-09-04 16:49:21 -0300

    implement-average-func

--- src/average.js
+++ src/average.js
@@ -1,3 +1,14 @@
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+
+  const areAllNumeric = array.every((value) => typeof value === 'number');
+
+  if (!areAllNumeric) return undefined;
+
+  const sum = array.reduce((acc, currentValue) => acc + currentValue, 0);
+  const avg = Math.round(sum / array.length);
+
+  return avg;
+};
 
 module.exports = average;
