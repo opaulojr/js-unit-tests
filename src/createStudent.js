dca0de58dfd981e0ca73394b00d85a76bdb451c8
opaulojr <paulojr.hs@gmail.com>
2023-09-04 17:15:21 -0300

    implement-createStudent-func

--- src/createStudent.js
+++ src/createStudent.js
@@ -1,3 +1,10 @@
-const createStudent = () => {};
+function createStudent(name) {
+  return {
+    name,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+}
 
 module.exports = createStudent;
